b1d0ac084ebafcd5390e0569e475b0fce2d027e0
ll <ll@ll.com>
2019-09-13 10:13:15 +0530

    ..

---
+++ alppat.py
@@ -0,0 +1,11 @@
+alp = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+       'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+j = 0
+k = 1
+while True:
+    i = 1
+    while k < i:
+        print(alp[k], end='')
+        k = k+1
+    j = j+2
+    i = i+j
---
+++ numpat.py
@@ -0,0 +1,8 @@
+n = 1
+while n <= 9:
+    k = 1
+    while k <= n:
+        print(n, end='')
+        k = k+1
+    print(end='\n')
+    n = n+1
