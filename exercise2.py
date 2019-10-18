112d4ff1350b64de55e271e80b9465ee7b9461e6
Avinash Rath <avinash.rrath@gmail.com>
2019-10-18 22:23:31 +0530

    Create exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,12 @@
+# Part A
+inp = input("Enter a string of length 4")
+a = ''
+for i in inp:
+  a += str(ord(i))
+print("The encoded values is ",a)
+
+#part B
+num = float(input('Enter the float'))
+print("Nearest rounded number ",round(n))
+print("Round off to 1st decimal place ",round(n,1))
+print("Round off to 2nd decimal place ",round(n,2))
