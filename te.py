7ba4e59179f933bd6315a359ec3cc55eb69f6f29
ll <ll@ll.com>
2019-10-18 10:26:04 +0530

    first

--- te.py
+++ te.py
@@ -1,11 +1,14 @@
 inp = input("enter the sentence: ")
 'inps = inp.strip()'
 inpsp = inp.split(" ")
-inpspp = inpsp
+leng = inpsp.__len__()
 i = 0
-for word in inpsp:
-    while i < inpspp.__len__():
-        if word == inpspp[i]:
-            inpspp.remove(word)
-        i=i+1
-print(inpspp)
+j = 0
+while i < leng:
+    j = i+1
+    while j < leng-1:
+        if inpsp[i] == inpsp[j]:
+            inpsp.remove(inpsp[i])
+        j = j+1
+    i = i+1
+print(inpsp)
