19b285cf7410ec24f55e0f4f321f3e715e262bcc
Anurag Verma <anuragv.1020@gmail.com>
2020-10-17 18:51:19 +0530

    Update 6c.py
    added bfs algorithm.

--- 6c.py
+++ 6c.py
@@ -1,6 +1,26 @@
-txt = ["hello", "hi", "welcome", "oyo", "gmail", "high", "qq"]
-count = 0
-for x in txt:
-    if len(x) >= 2 and x[0] == x[-1]:
-        count = count+1
-print(count)
+import collections
+
+# BFS algorithm
+def bfs(graph, root):
+
+    visited, queue = set(), collections.deque([root])
+    visited.add(root)
+
+    while queue:
+
+        # Dequeue a vertex from queue
+        vertex = queue.popleft()
+        print(str(vertex) + " ", end="")
+
+        # If not visited, mark it as visited, and
+        # enqueue it
+        for neighbour in graph[vertex]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
+
+
+if __name__ == '__main__':
+    graph = {0: [1, 2], 1: [2], 2: [3], 3: [1, 2]}
+    print("Following is Breadth First Traversal: ")
+    bfs(graph, 0)
